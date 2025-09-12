ec4939a5867c479e011867d3c880c14a47949ec8
Semen <semen.ruman@gmail.com>
2025-09-12 18:57:50 +0300

    Добавлена проверка на корректность ввода координат, когда ходит игрок

--- Sea_batle_game/player_motion.cpp
+++ Sea_batle_game/player_motion.cpp
@@ -2,12 +2,31 @@
 
 namespace play {
 	int* player_motion() { // ход игрока
-		int x, y;
-		int coords[2];
-		std::cout << "Ваш ход(введите координаты от 0 до 9): "<<std::endl;
-		std::cout << "Введите x: "; std::cin >> x; coords[0] = x;
-		std::cout << "Введите y: "; std::cin >> y; coords[1] = y;
+        static int coords[2]; // статический массив, чтобы вернуть указатель
+        int x, y;
 
-		return coords;
+        while (true) {
+            std::cout << "Ваш ход (введите координаты от 0 до 9):" << std::endl;
+
+            std::cout << "Введите x: ";
+            if (!(std::cin >> x) || x < 0 || x > 9) {
+                std::cout << "Некорректный ввод! Попробуйте снова.\n";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+            std::cout << "Введите y: ";
+            if (!(std::cin >> y) || y < 0 || y > 9) {
+                std::cout << "Некорректный ввод! Попробуйте снова.\n";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+            coords[0] = x;
+            coords[1] = y;
+            return coords;
+        }
 	}
 }
